fee94f6f3550c45e0eb3098de6c7fa281ef26fc4
Pianist-95 <web_sky_86@outlook.com>
2020-05-15 19:27:46 +0800

    Add new pages

---
+++ src/pages/contact.js
@@ -0,0 +1,10 @@
+import react from 'react'
+
+const ContactPage = () => {
+    return (
+        <div>
+            <h1>Contact</h1>
+            <p>The best way to reach me is via @ProfessorJudith on Telegram</p>
+        </div>
+    )
+}
