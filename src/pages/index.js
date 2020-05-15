1a7eb72b046ef95c1aea794e7115d983111936b6
Pianist-95 <web_sky_86@outlook.com>
2020-05-15 18:58:17 +0800

    Add Contact Link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,13 @@
 import React from "react"
+import { Link } from 'gatsby'
 
-export default () => <div>Hello world!</div>
+const IndexPage = () => {
+    return(
+        <div>
+            <h1>Hello!</h1>
+            <h2>I am WenTao from China. I have worked as Web developer for five years experience</h2>
+            <p>Need Senior Web Developer?<a href = "/contact">Contact Me</a></p>
+            <p>Need Senior Web Developer?<Link to = "/contact">Contact Me</Link></p>
+        </div>
+    )
+}
